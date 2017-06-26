9779548e67c832a98fd20f9f8ac253ab55066139
Russell Lenth <Russell Lenth>
2017-06-26 15:43:38 -0500

    Added copyrights, etc. Package builds OK, but there are still checking errors

--- R/examples.R
+++ R/examples.R
@@ -1,3 +1,24 @@
+##############################################################################
+#    Copyright (c) 2017 Russell V. Lenth                                     #
+#                                                                            #
+#    This file is part of the unrepx package for R (*unrepx*)                #
+#                                                                            #
+#    *unrepx* is free software: you can redistribute it and/or modify        #
+#    it under the terms of the GNU General Public License as published by    #
+#    the Free Software Foundation, either version 2 of the License, or       #
+#    (at your option) any later version.                                     #
+#                                                                            #
+#    *unrepx* is distributed in the hope that it will be useful,             #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#    GNU General Public License for more details.                            #
+#                                                                            #
+#    You should have received a copy of the GNU General Public License       #
+#    along with R and *unrepx*.  If not, see                                 #
+#    <https://www.r-project.org/Licenses/> and/or                            #
+#    <http://www.gnu.org/licenses/>.                                         #
+##############################################################################
+
 # Process development example, BH^2 (2nd ed) p. 200
 pdEff = structure(
     c(-8, 24, 1, -2.25, 0.75, -1.25, -0.75, -5.5, 0, 4.5, 
